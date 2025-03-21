9465f4885be8ea58fd779f5232ac26d41ac61247
Martynas Mazuolis <martynas.mazuolis@insoft.lt>
2025-03-21 10:34:19 +0200

    style: remove unused imports

--- bl/src/main/java/lt/restservice/bl/models/ThumbnailDtoList.java
+++ bl/src/main/java/lt/restservice/bl/models/ThumbnailDtoList.java
@@ -2,18 +2,11 @@ package lt.restservice.bl.models;
 
 import java.time.LocalDate;
 
-import org.hibernate.sql.results.internal.TupleElementImpl;
-
-import jakarta.persistence.Tuple;
-
-import jakarta.persistence.TupleElement;
-import jakarta.persistence.criteria.Path;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
 import lombok.extern.slf4j.Slf4j;
-import lt.restservice.model.Image_;
 
 @Slf4j
 @Data
